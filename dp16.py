28d473051e8bd9ff7949dccec5987cfcbf16135e
Durg143 <pp26012006@gmail.com>
2025-06-11 17:14:50 +0530

    Update dp16.py

--- dp16.py
+++ dp16.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import sklearn 
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
